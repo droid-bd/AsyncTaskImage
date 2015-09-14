150f61ddb243b4a5d15e3bec8b11d73d9029ae09
netcse <smamran09@gmail.com>
2015-09-14 18:11:52 +0600

    dynamic layout

--- app/src/main/java/com/netcse/asynctaskimage/MainActivity.java
+++ app/src/main/java/com/netcse/asynctaskimage/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -28,51 +29,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        ScrollView sv = new ScrollView(this);
+        // ScrollView sv = new ScrollView(this);
+        setContentView(R.layout.activity_main);
 
-        LinearLayout ll = new LinearLayout(this);
-
-        ll.setOrientation(LinearLayout.VERTICAL);
-
-        sv.addView(ll);
+        final LinearLayout lm = (LinearLayout) findViewById(R.id.linearMain);
 
 
 
-        TextView tv = new TextView(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-        tv.setText("Dynamic layouts ftw!");
+        //Create four
+        for(int j=0;j<=4;j++)
+        {
+            // Create LinearLayout
+            LinearLayout ll = new LinearLayout(this);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
 
-        ll.addView(tv);
+            // Create TextView
+            TextView product = new TextView(this);
+            product.setText(" Product"+j+"    ");
+            ll.addView(product);
 
+            // Create TextView
+            TextView price = new TextView(this);
+            price.setText("  $"+j+"     ");
+            ll.addView(price);
 
+            // Create Button
+            final Button btn = new Button(this);
+            // Give button an ID
+            btn.setId(j+1);
+            btn.setText("Add To Cart");
+            // set the layoutParams on the button
+            btn.setLayoutParams(params);
 
-        EditText et = new EditText(this);
+            final int index = j;
+            // Set click listener for button
+            btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 
-        et.setText("");
+                    Log.i("TAG", "index :" + index);
 
-        ll.addView(et);
+                    Toast.makeText(getApplicationContext(),
+                            "Clicked Button Index :" + index,
+                            Toast.LENGTH_LONG).show();
 
+                }
+            });
 
-
-        Button b = new Button(this);
-
-        b.setText("DButton");
-
-        ll.addView(b);
-
-
-
-        for(int i = 0; i < 200; i++) {
-
-            CheckBox cb = new CheckBox(this);
-
-            cb.setText("I'm dynamic!");
-
-            ll.addView(cb);
-
+            //Add button to LinearLayout
+            ll.addView(btn);
+            //Add button to LinearLayout defined in XML
+            lm.addView(ll);
         }
-
-        this.setContentView(sv);
 
     }
 
